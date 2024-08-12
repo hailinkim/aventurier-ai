50b2e6428d664deb3c974456b0a0d30d2723cbbb
hailinkim <haikim20@amherst.edu>
2024-08-12 12:21:08 +0900

    fix url

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 // import LoginForm from '@/components/LoginForm';
 import Link from 'next/link';
 export default async function Home() {
-  const response = await fetch('api/python');
+  const response = await fetch('http://127.0.0.1:5328/api/python');
   const data = await response.text();
   return (
       // <LoginForm/>
