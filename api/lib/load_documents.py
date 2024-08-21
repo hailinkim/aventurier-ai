ec399a5d8876afdce80eab57f4c70a5daf3ac076
hailinkim <haikim20@amherst.edu>
2024-08-22 00:08:02 +0900

    perf: parallelized document creation

--- api/lib/load_documents.py
+++ api/lib/load_documents.py
@@ -6,7 +6,9 @@ import re
 from typing import Dict
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import hashlib
-# import time
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 load_dotenv() 
 os.environ["UPSTAGE_API_KEY"] = os.getenv("UPSTAGE_API_KEY")
@@ -14,16 +16,20 @@ os.environ["MONGODB_ATLAS_CLUSTER_URI"] = os.getenv("NEXT_PUBLIC_MONGO_URI")
 client = MongoClient(os.environ["MONGODB_ATLAS_CLUSTER_URI"]) 
 
 def load_documents(username):
-    client = MongoClient(os.getenv("NEXT_PUBLIC_MONGO_URI"))
     db = client['instagram']
     user_collection = db['users'] 
-    user = user_collection.find({'username': username}, {"_id": 1})
+    user = user_collection.find_one({'username': username}, {"_id": 1})
 
-    for document in user:
-        user_id = document['_id']
+    DB_NAME = "langchain_db"
+    COLLECTION_NAME = "test"
+    langchain_db = client[DB_NAME]
+    vectorstore = langchain_db[COLLECTION_NAME]
+
+    # for document in user:
+    #     user_id = document['_id']
 
     post_collection = db['posts'] 
-    posts = post_collection.find({'user': user_id}, {'_id':1, 'caption': 1, 'location.name': 1, 'location.lng':1, 'location.lat':1})
+    posts = post_collection.find({'user': user["_id"]}, {'_id':1, 'caption': 1, 'location.name': 1, 'location.lng':1, 'location.lat':1})
     
     def generate_metadata(post: Dict) -> Dict:
         if 'location' in post:
@@ -43,7 +49,7 @@ def load_documents(username):
                 u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
                 u"\U00002700-\U000027BF"  # Dingbats
                 "]+", flags=re.UNICODE)
-        return emoji_pattern.sub(r'', text)
+        return emoji_pattern.sub(r'', text); 
     def generate_document_id(content, metadata):
         # Combine content and metadata into a single string
         combined_string = content + str(metadata)
@@ -51,24 +57,76 @@ def load_documents(username):
         document_id = hashlib.sha256(combined_string.encode('utf-8')).hexdigest()
         return document_id
 
-    document_ids = []
-    documents = []
-    # start = time.time()
-    for post in posts:
+    def process_post(post):
+        # if post['caption'] is None or post['caption'] == "":
+        #     return None
         metadata = generate_metadata(post)
         cleaned_caption = remove_emoji(post['caption'])
         cleaned_caption = cleaned_caption.replace("�", "")
+
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=500,
             chunk_overlap=20,
             length_function=len,
             is_separator_regex=False,
         )
+
         doc_list = text_splitter.split_text(cleaned_caption)
+        post_documents = []
+        post_document_ids = []
+
         for chunk in doc_list:
+            id = generate_document_id(chunk, metadata)
             document = Document(page_content=chunk, metadata=metadata)
-            documents.append(document)
-            document_ids.append(generate_document_id(chunk, metadata))
+            post_documents.append(document)
+            post_document_ids.append(id)
+
+        return post_documents, post_document_ids
+    
+    def process_posts_parallel(posts):
+        start = time.time()
+        documents = []
+        document_ids = []
+        futures = []
+        with ThreadPoolExecutor() as executor:
+            # futures = [executor.submit(process_post, post) for post in posts]
+            for post in posts:
+                found = vectorstore.find_one({"post_id":str(post["_id"])})
+                # add to futures only if the post is not found in the vector store and the caption is not empty
+                if not found and not (post['caption'] is None or post['caption'] != ""):
+                    futures.append(executor.submit(process_post, post))
+
+            for future in concurrent.futures.as_completed(futures):
+                result = future.result()
+                if result is not None:
+                    post_documents, post_document_ids = result
+                    documents.extend(post_documents)
+                    document_ids.extend(post_document_ids)
+        print("Time taken for splitting documents: ", time.time() - start)
+        return documents, document_ids
+    return process_posts_parallel(posts)
+
+    # sequential version
+    # document_ids = []
+    # documents = []
+    # start = time.time()
+    # for post in posts:
+    #     if post['caption'] is None or post['caption'] == "" or vectorstore.find_one({"post_id":str(post["_id"])}):
+    #         continue
+    #     metadata = generate_metadata(post)
+    #     cleaned_caption = remove_emoji(post['caption'])
+    #     cleaned_caption = cleaned_caption.replace("�", "")
+    #     text_splitter = RecursiveCharacterTextSplitter(
+    #         chunk_size=500,
+    #         chunk_overlap=20,
+    #         length_function=len,
+    #         is_separator_regex=False,
+    #     )
+    #     doc_list = text_splitter.split_text(cleaned_caption)
+    #     for chunk in doc_list:
+    #         document = Document(page_content=chunk, metadata=metadata)
+    #         documents.append(document)
+    #         document_ids.append(generate_document_id(chunk, metadata))
         
     # print("Time taken for splitting documents: ", time.time() - start)
-    return documents, document_ids
+    # return documents, document_ids
